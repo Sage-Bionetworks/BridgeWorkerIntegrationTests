2c17b2c68f78da5d9f728606bc0b4e8e09139342
imranmaj <imran.majeed@sagebase.org>
2022-09-16 15:14:48 -0700

    Finish test for study

--- src/test/java/org/sagebionetworks/bridge/exporter/integration/Exporter3Test.java
+++ src/test/java/org/sagebionetworks/bridge/exporter/integration/Exporter3Test.java
@@ -46,6 +46,7 @@ import org.sagebionetworks.repo.model.annotation.v2.AnnotationsValueType;
 import org.sagebionetworks.repo.model.sts.StsPermission;
 import org.sagebionetworks.repo.model.table.QueryResult;
 import org.sagebionetworks.repo.model.table.QueryResultBundle;
+import org.sagebionetworks.repo.model.table.Row;
 import org.sagebionetworks.repo.model.table.RowSet;
 import org.sagebionetworks.repo.model.table.SelectColumn;
 import org.slf4j.Logger;
@@ -76,6 +77,10 @@ import org.sagebionetworks.bridge.rest.model.AssessmentReference2;
 import org.sagebionetworks.bridge.rest.model.ClientInfo;
 import org.sagebionetworks.bridge.rest.model.Demographic;
 import org.sagebionetworks.bridge.rest.model.DemographicUser;
+import org.sagebionetworks.bridge.rest.model.DemographicUserAssessment;
+import org.sagebionetworks.bridge.rest.model.DemographicUserAssessmentStep;
+import org.sagebionetworks.bridge.rest.model.DemographicUserAssessmentStepAnswerType;
+import org.sagebionetworks.bridge.rest.model.DemographicUserResponse;
 import org.sagebionetworks.bridge.rest.model.Exporter3Configuration;
 import org.sagebionetworks.bridge.rest.model.HealthDataRecordEx3;
 import org.sagebionetworks.bridge.rest.model.ParticipantVersion;
@@ -107,9 +112,12 @@ public class Exporter3Test {
     private static final String CUSTOM_METADATA_KEY_SANITIZED = "custom_metadata_key";
     private static final String CUSTOM_METADATA_VALUE = "custom-metadata-value";
     private static final String STUDY_ID = "study1";
+    private static final String STUDY_2_ID = "study2";
     private static final byte[] UPLOAD_CONTENT = "This is the upload content".getBytes(StandardCharsets.UTF_8);
     private static final String WORKER_ID_BACKFILL_PARTICIPANTS = "BackfillParticipantVersionsWorker";
     private static final String WORKER_ID_REDRIVE_PARTICIPANTS = "RedriveParticipantVersionsWorker";
+    private static final String PARTICIPANT_VERSIONS_TABLE_ORDER_BY = "participantVersion";
+    private static final String PARTICIPANT_VERSIONS_DEMOGRAPHICS_VIEW_ORDER_BY = "participantVersion, appId, studyId, demographicCategoryName, demographicValue, demographicUnits";
 
     private static final String APP_NAME_FOR_USER = "app-name-for-user";
     private static final ClientInfo CLIENT_INFO_FOR_USER = new ClientInfo().appName(APP_NAME_FOR_USER);
@@ -119,6 +127,7 @@ public class Exporter3Test {
     private static Table ddbWorkerLogTable;
     private static Exporter3Configuration ex3Config;
     private static Exporter3Configuration ex3ConfigForStudy;
+    private static Exporter3Configuration ex3ConfigForStudy2;
     private static String rawDataBucket;
     private static S3Helper s3Helper;
     private static SqsHelper sqsHelper;
@@ -162,6 +171,7 @@ public class Exporter3Test {
         ForAdminsApi adminsApi = adminDeveloperWorker.getClient(ForAdminsApi.class);
         ex3Config = adminsApi.initExporter3().execute().body();
         ex3ConfigForStudy = adminsApi.initExporter3ForStudy(STUDY_ID).execute().body();
+        ex3ConfigForStudy2 = adminsApi.initExporter3ForStudy(STUDY_2_ID).execute().body();
     }
 
     @BeforeMethod
@@ -217,6 +227,15 @@ public class Exporter3Test {
         study.setExporter3Configuration(null);
         study.setExporter3Enabled(false);
         studiesApi.updateStudy(STUDY_ID, study).execute();
+
+        // Delete for study2.
+        Study study2 = studiesApi.getStudy(STUDY_2_ID).execute().body();
+        Exporter3Configuration ex3ConfigForStudy2 = study2.getExporter3Configuration();
+        deleteEx3Resources(ex3ConfigForStudy2);
+
+        study2.setExporter3Configuration(null);
+        study2.setExporter3Enabled(false);
+        studiesApi.updateStudy(STUDY_2_ID, study2).execute();
     }
 
     private static void deleteEx3Resources(Exporter3Configuration ex3Config) {
@@ -523,7 +542,17 @@ public class Exporter3Test {
     public void demographics() throws IOException, SynapseException, InterruptedException, TimeoutException {
         user.getClient(ForConsentedUsersApi.class)
                 .changeSharingScope(new SharingScopeForm().scope(SharingScope.ALL_QUALIFIED_RESEARCHERS)).execute();
-        user.getClient(DemographicsApi.class).saveDemographicUserSelf(STUDY_ID,
+        TestUser researcher = TestUserHelper.createAndSignInUser(Exporter3Test.class, false, Role.RESEARCHER);
+
+        // study save demographic user (version 2)
+        researcher.getClient(DemographicsApi.class).saveDemographicUser(STUDY_ID, user.getUserId(),
+                new DemographicUser().demographics(
+                    ImmutableMap.of(
+                            "category0",
+                            new Demographic().multipleSelect(true).values(ImmutableList.of("value0")))))
+                .execute().body();
+        // study overwrite demographic user self (version 3)
+        DemographicUserResponse overwritingDemographicUser = user.getClient(DemographicsApi.class).saveDemographicUserSelf(STUDY_ID,
                 new DemographicUser().demographics(
                         ImmutableMap.of(
                                 "category1",
@@ -531,68 +560,111 @@ public class Exporter3Test {
                                 "category2",
                                 new Demographic().multipleSelect(false).values(ImmutableList.of("value1"))
                                         .units("units1"))))
-                .execute();
+                .execute().body();
+        // study delete demographic (version 4)
+        researcher.getClient(DemographicsApi.class).deleteDemographic(STUDY_ID, user.getUserId(),
+                overwritingDemographicUser.getDemographics().get("category1").getId()).execute();
+        // app save demographics assessment (version 5)
+        adminDeveloperWorker.getClient(DemographicsApi.class)
+                .saveDemographicUserAssessmentAppLevel(user.getUserId(),
+                        new DemographicUserAssessment().stepHistory(ImmutableList.of(new DemographicUserAssessmentStep()
+                                .answerType(new DemographicUserAssessmentStepAnswerType().type("array"))
+                                .identifier("category3")
+                                .value(ImmutableList.of("value2", "value3")))))
+                .execute().body();
+        // account update (should trigger demographics export also) (version 6)
+        StudyParticipant participant = adminDeveloperWorker.getClient(ParticipantsApi.class).getParticipantById(user.getUserId(), false).execute().body();
+        participant.setClientTimeZone("America/Los_Angeles");
+        adminDeveloperWorker.getClient(ParticipantsApi.class).updateParticipant(user.getUserId(), participant).execute();
+        // study delete demographic user (only app demographics should remain) (version 7)
+        researcher.getClient(DemographicsApi.class).deleteDemographicUser(STUDY_ID, user.getUserId()).execute();
 
         // wait for export
-        Thread.sleep(25000);
+        Thread.sleep(30000);
 
+        // fetch synapse tables
         Map<String, String> tableIdToOrderByClause = new HashMap<>();
-        tableIdToOrderByClause.put(ex3ConfigForStudy.getParticipantVersionTableId(), "participantVersion");
+        // participant version tables
+        tableIdToOrderByClause.put(ex3Config.getParticipantVersionTableId(), PARTICIPANT_VERSIONS_TABLE_ORDER_BY);
+        tableIdToOrderByClause.put(ex3ConfigForStudy.getParticipantVersionTableId(), PARTICIPANT_VERSIONS_TABLE_ORDER_BY);
+        tableIdToOrderByClause.put(ex3ConfigForStudy2.getParticipantVersionTableId(), PARTICIPANT_VERSIONS_TABLE_ORDER_BY);
+        // demographics tables
+        tableIdToOrderByClause.put(ex3Config.getParticipantVersionDemographicsTableId(), null);
         tableIdToOrderByClause.put(ex3ConfigForStudy.getParticipantVersionDemographicsTableId(), null);
+        tableIdToOrderByClause.put(ex3ConfigForStudy2.getParticipantVersionDemographicsTableId(), null);
+        // joined participant versions and demographics tables
+        tableIdToOrderByClause.put(ex3Config.getParticipantVersionDemographicsViewId(),
+                PARTICIPANT_VERSIONS_DEMOGRAPHICS_VIEW_ORDER_BY);
         tableIdToOrderByClause.put(ex3ConfigForStudy.getParticipantVersionDemographicsViewId(),
-                "appId, studyId, participantVersion, demographicCategoryName, demographicValue, demographicUnits");
+                PARTICIPANT_VERSIONS_DEMOGRAPHICS_VIEW_ORDER_BY);
+        tableIdToOrderByClause.put(ex3ConfigForStudy2.getParticipantVersionDemographicsViewId(),
+                PARTICIPANT_VERSIONS_DEMOGRAPHICS_VIEW_ORDER_BY);
         Map<String, QueryResultBundle> queryResults = selectAllFromSynapseTablesWithQueryCount(tableIdToOrderByClause,
                 100L);
 
-        // get demographics version (which is version 2 at index 1)
+        // get participant versions
         QueryResultBundle participantVersionTableResults = queryResults
                 .get(ex3ConfigForStudy.getParticipantVersionTableId());
         QueryResult participantVersionTableResult = participantVersionTableResults.getQueryResult();
-        List<String> demographicsParticipantVersionRow = participantVersionTableResult.getQueryResults().getRows()
-                .get(1).getValues();
+        List<Row> participantVersionRows = participantVersionTableResult.getQueryResults().getRows();
 
         // check demographics table
+        // don't need to check the values in the table because they are included in the view
         QueryResultBundle demographicsTableResults = queryResults
                 .get(ex3ConfigForStudy.getParticipantVersionDemographicsTableId());
         // check demographics table size
-        assertEquals(demographicsTableResults.getQueryCount().longValue(), 2L);
-        List<String> expectedColumnNames = ImmutableList.of("healthCode", "participantVersion", "appId", "studyId",
-                "demographicCategoryName", "demographicValue", "demographicUnits");
+        assertEquals(demographicsTableResults.getQueryCount().longValue(), 12L);
         // check demographics table column names
-        assertEquals(demographicsTableResults.getQueryResult().getQueryResults().getHeaders().size(),
-                expectedColumnNames.size());
-        for (int i = 0; i < expectedColumnNames.size(); i++) {
-            assertEquals(demographicsTableResults.getQueryResult().getQueryResults().getHeaders().get(i).getName(),
-                    expectedColumnNames.get(i));
+        List<String> expectedDemographicTableColumnNames = ImmutableList.of("healthCode", "participantVersion", "appId", "studyId", "demographicCategoryName", "demographicValue", "demographicUnits");
+        assertEquals(demographicsTableResults.getQueryResult().getQueryResults().getHeaders().size(), expectedDemographicTableColumnNames.size());
+        for (int i = 0; i < expectedDemographicTableColumnNames.size(); i++) {
+            assertEquals(demographicsTableResults.getQueryResult().getQueryResults().getHeaders().get(i).getName(), expectedDemographicTableColumnNames.get(i));
         }
 
         // check demographics view
         QueryResultBundle demographicsViewResults = queryResults
                 .get(ex3ConfigForStudy.getParticipantVersionDemographicsViewId());
         // check demographics view size
-        assertEquals(demographicsViewResults.getQueryCount().longValue(), 2L);
+        assertEquals(demographicsViewResults.getQueryCount().longValue(), 12L);
         QueryResult demographicsViewResult = demographicsViewResults.getQueryResult();
+        // check demographics view column names
+        List<String> expectedDemographicViewColumnNames = ImmutableList.of("healthCode", "participantVersion", "createdOn", "modifiedOn", "dataGroups", "languages", "sharingScope", "studyMemberships", "clientTimeZone", "appId", "studyId", "demographicCategoryName", "demographicValue", "demographicUnits");
+        assertEquals(demographicsViewResults.getQueryResult().getQueryResults().getHeaders().size(), expectedDemographicViewColumnNames.size());
+        for (int i = 0; i < expectedDemographicViewColumnNames.size(); i++) {
+            assertEquals(demographicsViewResults.getQueryResult().getQueryResults().getHeaders().get(i).getName(), expectedDemographicViewColumnNames.get(i));
+        }
         // check demographics view values
-        List<String> expectedRow0 = new ArrayList<>(demographicsParticipantVersionRow);
-        expectedRow0.add(IntegTestUtils.TEST_APP_ID);
-        expectedRow0.add(STUDY_ID);
-        expectedRow0.add("category1");
-        expectedRow0.add(null);
-        expectedRow0.add(null);
-        assertEquals(demographicsViewResult.getQueryResults().getRows().get(0).getValues(), expectedRow0);
-        List<String> expectedRow1 = new ArrayList<>(demographicsParticipantVersionRow);
-        expectedRow1.add(IntegTestUtils.TEST_APP_ID);
-        expectedRow1.add(STUDY_ID);
-        expectedRow1.add("category2");
-        expectedRow1.add("value1");
-        expectedRow1.add("units1");
-        assertEquals(demographicsViewResult.getQueryResults().getRows().get(1).getValues(), expectedRow1);
+        List<Row> demographicsViewResultRows = demographicsViewResult.getQueryResults().getRows();
+        // version 2
+        assertEquals(demographicsViewResultRows.get(0).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 2, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category0", "value0", null));
+        // version 3
+        assertEquals(demographicsViewResultRows.get(1).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 3, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category1", null, null));
+        assertEquals(demographicsViewResultRows.get(2).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 3, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category2", "value1", "units1"));
+        // version 4
+        assertEquals(demographicsViewResultRows.get(3).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 4, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category2", "value1", "units1"));
+        // version 5
+        assertEquals(demographicsViewResultRows.get(4).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 5, IntegTestUtils.TEST_APP_ID, null, "category3", "value2", null));
+        assertEquals(demographicsViewResultRows.get(5).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 5, IntegTestUtils.TEST_APP_ID, null, "category3", "value3", null));
+        assertEquals(demographicsViewResultRows.get(6).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 5, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category2", "value1", "units1"));
+        // version 6
+        assertEquals(demographicsViewResultRows.get(7).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 6, IntegTestUtils.TEST_APP_ID, null, "category3", "value2", null));
+        assertEquals(demographicsViewResultRows.get(8).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 6, IntegTestUtils.TEST_APP_ID, null, "category3", "value3", null));
+        assertEquals(demographicsViewResultRows.get(9).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 6, IntegTestUtils.TEST_APP_ID, STUDY_ID, "category2", "value1", "units1"));
+        // version 7
+        assertEquals(demographicsViewResultRows.get(10).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 7, IntegTestUtils.TEST_APP_ID, null, "category3", "value2", null));
+        assertEquals(demographicsViewResultRows.get(11).getValues(), makeExpectedDemographicsViewRow(participantVersionRows, 7, IntegTestUtils.TEST_APP_ID, null, "category3", "value3", null));
+    }
 
-        // study save demographic user
-        // study delete demographic
-        // study delete demographic user
-        // app overwrite demographics
-        // account update
+    private List<String> makeExpectedDemographicsViewRow(List<Row> participantVersionRows, int versionNum, String appId, String studyId, String categoryName, String value, String units) {
+        // the first part of each row is the same as in the participant versions table
+        // participant versions are indexed from 1
+        List<String> expectedRow = new ArrayList<>(participantVersionRows.get(versionNum - 1).getValues());
+        expectedRow.add(appId);
+        expectedRow.add(studyId);
+        expectedRow.add(categoryName);
+        expectedRow.add(value);
+        expectedRow.add(units);
+        return expectedRow;
     }
 
     /**
@@ -636,12 +708,14 @@ public class Exporter3Test {
                     QueryResultBundle queryResult = synapseClient.queryTableEntityBundleAsyncGet(jobId, tableId);
                     // if we get here the query is complete
                     queryResults.put(tableId, queryResult);
+                    // remove it so we don't try to poll it again
                     iter.remove();
                 } catch (SynapseResultNotReadyException e) {
 
                 }
 
                 if (tableIdToJobId.isEmpty()) {
+                    // no more jobs left to wait on
                     return queryResults;
                 }
                 Thread.sleep(500);
